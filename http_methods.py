0f0563f66fc9c951a17de22789420d61edff60f4
Pedro Saint Clair Garcia <pedro.garcia@take.net>
2022-12-15 13:19:59 -0300

    http methods

--- http_methods.py
+++ http_methods.py
@@ -1,16 +1,22 @@
 from flask import Flask
 from flask import request
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+'''@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         return do_the_login()
     else:
         return show_the_login_form()
 
-'''@app.get('/login')
+@app.get('/login')
 def login_get():
     return show_the_login_form()
 
--- route.py
+++ route.py
@@ -48,7 +48,13 @@ if __name__ == '__main__':
     app.run(debug=True)
 
     with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('login', next='/'))
-    print(url_for('profile', username='John Doe'))
+        print(url_for('index'))
+        print(url_for('login'))
+        print(url_for('login', next='/'))
+        print(url_for('profile', username='John Doe'))
+
+    with app.test_request_context('/hello', method='POST'):
+        # now you can do something with the request until the
+        # end of the with block, such as basic assertions:
+        assert request.path == '/hello'
+        assert request.method == 'POST'
